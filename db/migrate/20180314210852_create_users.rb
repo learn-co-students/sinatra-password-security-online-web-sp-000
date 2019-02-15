6378d797469a45bd7a56e5227448d1d8ba7c87f5
Cookie McCormick <muntananuchat@gmail.com>
2019-02-15 16:12:43 -0500

    Done.

--- db/migrate/20180314210852_create_users.rb
+++ db/migrate/20180314210852_create_users.rb
@@ -1,7 +1,13 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def up
+    create_table :users do |t|
+      t.string :username, null: false
+      t.string :password_digest, null: false
+      t.timestamps null: false
+    end
   end
-  
+
   def down
+    drop_table :users
   end
 end
