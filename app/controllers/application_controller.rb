0d1fdf451f060854611615c92021a8299b1cae66
Gregory Losco <gtlosco@gmail.com>
2020-01-17 01:53:16 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -35,7 +35,7 @@ post "/login" do
   user = User.find_by(:username => params[:username])
  
   if user && user.authenticate(params[:password])
-    session[:user_id] = user.id 
+    session[:id] = user.id 
     redirect "/success"
   else
     redirect "/failure"
