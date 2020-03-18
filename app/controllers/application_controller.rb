96c9609e5ae661f1ca186353ffd1dfad75292e23
Michael Carroll <carrollm2@gmail.com>
2020-03-18 19:46:18 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -36,7 +36,6 @@ class ApplicationController < Sinatra::Base
 		user = User.find_by(:username => params[:username])
 
 	  if user && user.authenticate(params[:password])
-      binding.pry
 	    session[:user_id] = user.id
 	    redirect "/success"
 	  else
@@ -45,6 +44,8 @@ class ApplicationController < Sinatra::Base
 	end
 
 	get "/success" do
+		current_user
+		
 		if logged_in?
 			erb :success
 		else
