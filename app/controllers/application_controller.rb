b10fcbc510961a1032e09362193815608d27a168
Winston Pemberton <winstonpemberton@gmail.com>
2020-03-27 22:22:54 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,58 +1,72 @@
 require "./config/environment"
 require "./app/models/user"
+require 'pry'
 class ApplicationController < Sinatra::Base
 
-	configure do
-		set :views, "app/views"
-		enable :sessions
-		set :session_secret, "password_security"
-	end
+  configure do
+    set :views, "app/views"
+    enable :sessions
+    set :session_secret, "password_security"
+  end
 
-	get "/" do
-		erb :index
-	end
+  get "/" do
+    erb :index
+  end
 
-	get "/signup" do
-		erb :signup
-	end
+  get "/signup" do
+    erb :signup
+  end
 
 	post "/signup" do
-		#your code here!
+	  user = User.new(:username => params[:username], :password => params[:password])
+
+	  if user.save
+	    redirect "/login"
+	  else
+	    redirect "/failure"
+	  end
 	end
 
-	get "/login" do
-		erb :login
-	end
+  get "/login" do
+    erb :login
+  end
 
-	post "/login" do
-		#your code here!
-	end
+  post "/login" do
+    user = User.find_by(:username => params[:username])
 
-	get "/success" do
-		if logged_in?
-			erb :success
+		if user && user.authenticate(params[:password])
+			session[:user_id] = user.id
+			redirect to "/success"
 		else
-			redirect "/login"
+			redirect to "/failure"
 		end
-	end
+  end
 
-	get "/failure" do
-		erb :failure
-	end
+  get "/success" do
+    if logged_in?
+      erb :success
+    else
+      redirect to "/login"
+    end
+  end
 
-	get "/logout" do
-		session.clear
-		redirect "/"
-	end
+  get "/failure" do
+    erb :failure
+  end
 
-	helpers do
-		def logged_in?
-			!!session[:id]
-		end
+  get "/logout" do
+    session.clear
+    redirect to "/"
+  end
 
-		def current_user
-			User.find(session[:id])
-		end
-	end
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+  end
 
 end
