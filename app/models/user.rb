7868f9d3fe6feb8b8037973cc47daa7f8f3ed2bd
Taishawnk <Taishawnking1@yahoo.com>
2020-06-24 11:15:53 -0600

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
 class User < ActiveRecord::Base
-	
+	has_secure_password
 end
+
+
+post 'login' do 
+	user= User.find_by(:username => params[:username])
+	session[:user_id]
+	if user && user.athenticate(params[:password])
+		redirect '/sucsess'
+	else 
+		redirect '/fail'	
+		end
+	end
