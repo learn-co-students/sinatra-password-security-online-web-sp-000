550f1a0c8303028afcf9a08aae5f40905e2cd422
github username <github email address>
2020-11-15 19:28:25 -0500

    Done.

--- db/migrate/20180314210852_create_users.rb
+++ db/migrate/20180314210852_create_users.rb
@@ -1,10 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def up
-  end
     create_table :users do |t|
       t.string :username
       t.string :password_digest
     end
+  end
   def down
     drop_table :users
   end
