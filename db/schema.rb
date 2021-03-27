fbbf8441c892d7dad31c4cc2377d4a0fa424dd78
lahb2434 <chrishoughton2434@gmail.com>
2021-03-26 21:37:23 -0400

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180314210852) do
+ActiveRecord::Schema.define(version: 0) do
 
 end
